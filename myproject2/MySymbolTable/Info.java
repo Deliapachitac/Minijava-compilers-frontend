fd2f32a5f69cd9cf0173dfa4f897a9b58f5e4593
Deliapachitac <deliapachitacmaria@gmail.com>
2026-05-12 09:57:28 +0300

    completed info data structure

--- myproject2/MySymbolTable/Info.java
+++ myproject2/MySymbolTable/Info.java
@@ -21,12 +21,11 @@ public class Info {
     }
 
 
-    public void addVariable(String type, String variablename) {
+    public void addVariable(String type, String variablename)throws  Exception{
         // Check if the variable already exists in the current class
         for (String[] current : variables) {
             if (current[1].equals(variablename)) {
-                System.out.println("Variable already exists" );
-                return;
+                throw new Exception("Variable already exists" );
             }
         }
         
@@ -35,16 +34,17 @@ public class Info {
         variables.add(new_variable);
     }
 
-    public void addMethod(String methodName, String returnType) {
+    public void addMethod(String methodName, String returnType ,LinkedList<String[]> parameters)throws Exception {
         // Check if the method already exists in the current class
-        for (MethodInfo current : methods) {
-            if (current.name.equals(methodName)) {
-                System.out.println("Method already exists" );
-                return;
-            }
-        }
+        String key =MethodInfo.getKey(methodName, parameters);
+        for (MethodInfo current : methods){
+            if (current.getKey().equals(key)){
+                throw new Exception("Method already exists" );    
+                  
+           }
+        }             
 
-        MethodInfo new_method = new MethodInfo(methodName, returnType);
+        MethodInfo new_method =new MethodInfo(methodName, returnType);
         methods.add(new_method);
     }
 
@@ -52,3 +52,56 @@ public class Info {
 }
 
 
+class MethodInfo {
+    public String name;
+    public String returnType;
+    public LinkedList<String[]> parameters; // list of parameters [type, name]
+    public LinkedList<String[]> localVariables; // list of local variables [type, name]
+
+    public MethodInfo(String name, String returnType) {
+        this.name = name;
+        this.returnType = returnType;
+
+        parameters = new LinkedList<String[]>();
+        localVariables = new LinkedList<String[]>();
+        
+    }
+
+    public void addParameter(String type, String parameterName)throws Exception{
+        // Check if the parameter already exists in the current method
+        for (String[] current : parameters) {
+            if (current[1].equals(parameterName)) {
+                throw new Exception("Parameter already exists" );
+              
+            }
+        }         
+
+        String[] new_parameter = new String[]{type, parameterName};
+        parameters.add(new_parameter);
+    }
+
+    public void addLocalVariable(String type, String variableName)throws Exception {
+        // Check if the local variable already exists in the current method
+        for (String[] current : localVariables) {
+            if (current[1].equals(variableName)) {
+                throw new Exception("Local variable already exists" );
+                
+            }
+        }         
+
+        String[] new_local_variable = new String[]{type, variableName};
+        localVariables.add(new_local_variable);
+    }
+
+    
+    public static String getKey(String name, LinkedList<String[]> parameters) {
+        String key= new String(name);
+        for (String[] p: parameters) {
+            key += "_" + p[0]; 
+        }
+        return key;
+    }
+    public String getKey(){
+        return getKey(this.name,this.parameters);
+    }      
+}
